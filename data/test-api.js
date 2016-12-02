c07073a3ff4374a79208e69e3be09674d5061fd5
Nicholas Giordano <nicholas.giordano@live.com>
2016-12-02 13:07:28 +0100

    Update test-api.js

--- data/test-api.js
+++ data/test-api.js
@@ -2,10 +2,20 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 
+//questa funzione get restituisce la lista totale delle biblioteche
 app.get('/listaBiblioteche', function (req, res) {
    fs.readFile( __dirname + "/" + "biblioteche.json", 'utf8', function (err, data) {
        console.log( data );
        res.end( data );
+   });
+})
+//questa funzione restituisce la lista delle informazioni per una determinata biblioteca
+app.get('/:id', function (req, res) {
+   fs.readFile( __dirname + "/" + "biblioteche.json", 'utf8', function (err, data) {
+       biblioteche = JSON.parse( data );
+       var biblioteca = biblioteche["biblioteca" + req.params.id] 
+       console.log( biblioteca );
+       res.end( JSON.stringify(biblioteca));
    });
 })
 
