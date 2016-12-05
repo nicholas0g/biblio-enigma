491560d391b83e333dbffd3091c68428cc1050ce
Nicholas Giordano <nicholas.giordano@live.com>
2016-12-05 12:24:25 +0100

    Funzione posti occupati inserita (demo)

--- server-test/serverRest.js
+++ server-test/serverRest.js
@@ -3,11 +3,20 @@ var app = express();
 var https=require('https');
 var fs = require("fs");
 
+//durante l'inizializzazione del servizio tutti i posti sono liberi
+var posti={1:0,2:0,3:0,4:0,5:0};
+var ok={"result":"ok"};
+var fail={"result":"fail"};
+
 //utilizzato per risolvere problematiche CORS
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
+});
+
+app.get('/postiOccupati',function(req,res){
+    res.end( JSON.stringify(posti));
 });
 
 //questa funzione get restituisce la lista totale delle biblioteche
@@ -26,6 +35,32 @@ app.get('/:id', function (req, res,next) {
        res.end( JSON.stringify(biblioteca));
    });
 })
+
+app.post('/studenteIn/:id',function(req,res){
+    fs.readFile( __dirname + "/" + "biblioteche.json", 'utf8', function (err, data) {
+       biblioteche = JSON.parse( data );
+       var residui = biblioteche["biblioteca" + req.params.id]['postazioni-studio'];
+        console.log("Studente in ingresso sulla biblioteca %s",req.params.id)
+        if(posti[req.params.id]<residui){
+            posti[req.params.id]=posti[req.params.id]+1;
+            res.end(JSON.stringify(ok));
+        }
+        else res.end(JSON.stringify(fail));
+   });
+});
+
+app.post('/studenteOut/:id',function(req,res){
+    fs.readFile( __dirname + "/" + "biblioteche.json", 'utf8', function (err, data) {
+       biblioteche = JSON.parse( data );
+       var residui = biblioteche["biblioteca" + req.params.id]['postazioni-studio'];
+        console.log("Studente in uscita sulla biblioteca %s",req.params.id)
+        if(posti[req.params.id]>0){
+            posti[req.params.id]=posti[req.params.id]-1;
+            res.end(JSON.stringify(ok));
+        }
+        else res.end(JSON.stringify(fail));
+   });
+});
 
 app.get('/', function (req, res,next) {
        console.log('Richiesta GET vuota');
