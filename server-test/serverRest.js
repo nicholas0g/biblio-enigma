1c2f0479d78475401677a051ccad4de92951696c
Nicholas Giordano <nicholas.giordano@live.com>
2016-12-05 09:50:22 +0100

    Risolte le problematiche di CORS delle API
    Modificata index con test di chiamata api

--- server-test/serverRest.js
+++ server-test/serverRest.js
@@ -1,16 +1,24 @@
 var express = require('express');
 var app = express();
+var https=require('https');
 var fs = require("fs");
 
+//utilizzato per risolvere problematiche CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 //questa funzione get restituisce la lista totale delle biblioteche
-app.get('/listaBiblioteche', function (req, res) {
+app.get('/listaBiblioteche', function (req, res,next) {
    fs.readFile( __dirname + "/" + "biblioteche.json", 'utf8', function (err, data) {
        console.log('Richiesta GET generica');
        res.end( data );
    });
 })
 //questa funzione restituisce la lista delle informazioni per una determinata biblioteca
-app.get('/:id', function (req, res) {
+app.get('/:id', function (req, res,next) {
    fs.readFile( __dirname + "/" + "biblioteche.json", 'utf8', function (err, data) {
        biblioteche = JSON.parse( data );
        var biblioteca = biblioteche["biblioteca" + req.params.id] 
@@ -19,10 +27,12 @@ app.get('/:id', function (req, res) {
    });
 })
 
-app.get('/', function (req, res) {
+app.get('/', function (req, res,next) {
        console.log('Richiesta GET vuota');
        res.end( ':D - greetings from www.nichoalsgiordano.it');
 })
+
+
 
 var server = app.listen(8081, function () {
 
@@ -32,3 +42,16 @@ var server = app.listen(8081, function () {
   console.log("Server di test in ascolto su http://%s:%s", host, port)
 
 })
+
+/*
+codice per un servizio https over ssl cert!
+//array contenente certificati self-signed di test
+var options = {
+  key: fs.readFileSync('test-certificate/20623981-localhost.key'),
+  cert: fs.readFileSync('test-certificate/20623981-localhost.cert')
+};
+var server=https.createServer(options,app);
+server.listen(8081, function(){
+        console.log("Servizio https.ssl.selfsigned in esecuzione su localhost:8081")
+    });
+*/
